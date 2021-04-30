86fdf74a0621faf4db5e312ceb05b62c02cd1522
Luis Javier López Arredondo <ljDevelopment.6.28@gmail.com>
2021-04-30 23:04:29 +0200

    Movies raw card.

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -5,12 +5,21 @@ import './MovieList.css';
 
 class MovieList extends React.Component {
 
+
+
 	render() {
+		console.log(this.props.movies);
 		return (
 			<div className='movies'>
 				<ul>
 				{this.props.movies.map((c, i) => (
-					<li key={c.id}>{c.title}</li>
+					<li key={c.id}>
+						<div>{c.title}</div>
+						<div>{c.original_title}</div>
+						<div>{c.release_date}</div>
+						<div><img src={'https://www.themoviedb.org/t/p/w94_and_h141_bestv2/' + c.poster_path} /></div>
+						<div>{c.overview}</div>
+					</li>
 							))}
 				</ul>
 			</div>
