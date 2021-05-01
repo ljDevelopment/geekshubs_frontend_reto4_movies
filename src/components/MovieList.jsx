113ee9f6a95d6eb7f46bc615d3b5433106c3b043
Luis Javier López Arredondo <ljDevelopment.6.28@gmail.com>
2021-05-02 00:37:52 +0200

    Movies cards layout

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -13,12 +13,12 @@ class MovieList extends React.Component {
 			<div className='movies'>
 				<ul>
 				{this.props.movies.map((c, i) => (
-					<li key={c.id}>
-						<div>{c.title}</div>
-						<div>{c.original_title}</div>
-						<div>{c.release_date}</div>
-						<div><img src={'https://www.themoviedb.org/t/p/w94_and_h141_bestv2/' + c.poster_path} /></div>
-						<div>{c.overview}</div>
+					<li key={c.id} class="movie-card">
+						<header>{c.title}</header>
+						<div class="subtitle">({c.original_title})</div>
+						<div class="subtitle2">{c.release_date}</div>
+						<div><img src={'https://www.themoviedb.org/t/p/w94_and_h141_bestv2/' + (c.poster_path || c.backdrop_path)} /></div>
+						<article class="overflowed">{c.overview}</article>
 					</li>
 							))}
 				</ul>
